f78a530792919bf6d4a5f0fee7d8d4fd4c708b77
Vũ Đức Trung <19520324@gm.uit.edu.vn>
2020-10-15 22:00:01 +0700

    Delete file vs

--- Coffee_Shop_Management/Sale/Program.cs
+++ Coffee_Shop_Management/Sale/Program.cs
@@ -17,20 +17,20 @@ namespace Sale
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Sell());
-            Application.Run(new KhachHang_ThongTin());
-            Application.Run(new KhachHang_ThongTin_ChinhSua());
-            Application.Run(new XemThongBao());
-            Application.Run(new ThongTinTaiChinh());
-            Application.Run(new FormThucDon());
-            Application.Run(new FormThemMon());
-            Application.Run(new FormSuaMon());
-            Application.Run(new FormXoaMon());
-            Application.Run(new FormThemUser());
-            Application.Run(new FormChangeInfoUser());
-            Application.Run(new FormXoaUser());
-            Application.Run(new FormThongKe());
-            Application.Run(new FormKetQuaThongKe());
+            //Application.Run(new Sell());
+            //Application.Run(new KhachHang_ThongTin());
+            //Application.Run(new KhachHang_ThongTin_ChinhSua());
+            //Application.Run(new XemThongBao());
+            //Application.Run(new ThongTinTaiChinh());
+            //Application.Run(new FormThucDon());
+            //Application.Run(new FormThemMon());
+            //Application.Run(new FormSuaMon());
+            //Application.Run(new FormXoaMon());
+            //Application.Run(new FormThemUser());
+            //Application.Run(new FormChangeInfoUser());
+            //Application.Run(new FormXoaUser());
+            //Application.Run(new FormThongKe());
+            //Application.Run(new FormKetQuaThongKe());
             Application.Run(new FormLogin());
         }
     }
